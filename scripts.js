4dcb2c53e04b1ebfcd8e63b0ee886fbfe5751164
aiman <s67516@ocean.umt.edu.my>
2025-01-01 13:08:44 +0800

    created full homepage without recipe

--- scripts.js
+++ scripts.js
@@ -1,12 +1,19 @@
 headerBackground = document.querySelector(".intro");
 
 document.addEventListener("DOMContentLoaded", () => {
+  const toggler = document.getElementById("toggler");
+  const navBar = document.getElementById("nav-bar");
+
+  toggler.addEventListener("click", function () {
+    navBar.classList.toggle("active");
+  });
+
   changeHeaderBackground();
 });
 
 let colorNow = 0;
 function changeHeaderBackground() {
-  headerBackground.style.background = `url("/assets/background/${
+  headerBackground.style.background = `url("assets/background/${
     colorNow + 1
   }.png") no-repeat center center/cover`;
   setTimeout(() => {
